51af765f5ddfb432e1a381ee0d326c7a36e9acd6
Heutras <alperceltr@gmail.com>
2022-07-24 23:33:29 +0300

    imageSlider
    imageSlider added at the phone in the login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 function App() {
 
   const ref = useRef();
+
+  useEffect(() => {
+    let images = ref.current.querySelectorAll('img'),
+    total = images.length,
+    current = 0;
+    const imageSlider = () => {
+      if( current > 0) {
+        images[current -1].classList.add('opacity-0');
+      }
+      else {
+        images[total -1].classList.add('opacity-0');
+      }
+      images[current].classList.remove('opacity-0');
+      if( current === total - 1 ){
+        current = 0;
+      } else {
+        current += 1;
+      }
+    };
+    imageSlider();
+    let interval = setInterval(imageSlider, 3000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [ref])
   
   return (
     <div className="h-full w-full flex items-center justify-center">
       <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
-        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]">
-          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png" alt="ss1"/>
-          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png" alt="ss1"/>
-          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png" alt="ss1"/>
-          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png" alt="ss1"/>
+        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1.png/fdfe239b7c9f.png" alt="ss1"/>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3.png/94edb770accf.png" alt="ss1"/>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2.png/4d62acb667fb.png" alt="ss1"/>
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4.png/a4fd825e3d49.png" alt="ss1"/>
         </div>
       </div>
-    </div>
-  );
+    </div> );
 }
 
 export default App;
