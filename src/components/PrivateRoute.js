db720c2c5ba280a61aff095579aad9386c9ddbcb
Heutras <alperceltr@gmail.com>
2022-07-26 02:59:56 +0300

    button, search component added
    changed

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,17 @@
-import { Navigate, useLocation} from "react-router-dom";
-import { useSelector } from "react-redux/es/exports";
-export default function PrivateRoute( {children} ) {
-    const user = useSelector(state => state.auth.user);
-    const location = useLocation();
-    if(!user) {
-        return <Navigate to="/auth/login" replace={true} state={{
-            return_url: location.pathname
-        }}/>
-    }
-    return children;
+import {Navigate, useLocation} from "react-router-dom"
+import {useSelector} from "react-redux";
+
+export default function PrivateRoute({ children }) {
+
+	const user = useSelector(state => state.auth.user)
+	const location = useLocation()
+
+	if (!user) {
+		return <Navigate to="/auth/login" replace={true} state={{
+			return_url: location.pathname
+		}} />
+	}
+
+	return children
+
 }
