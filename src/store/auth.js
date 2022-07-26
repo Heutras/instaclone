c09c2f5c153125cd1f3366bacf9f23db66636bde
Heutras <alperceltr@gmail.com>
2022-07-26 03:27:01 +0300

    user reducer updated

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,23 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit'
+import {createSlice} from "@reduxjs/toolkit";
+
 const initialState = {
-    user : false
+	user: null
 }
+
 const auth = createSlice({
-    name:'auth',
-    initialState,
-    reducers: {
-        setUser: (state, action) => {
-            if(action.payload){
-                localStorage.setItem('user', JSON.stringify(action.payload))
-            }
-            else {
-                localStorage.removeItem('user')
-            }
-            state.user = action.payload
-        }
-    }
-});
+	name: 'auth',
+	initialState,
+	reducers: {
+		setUser: (state, action) => {
+			state.user = action.payload
+		}
+	}
+})
 
-export const { setUser } = auth.actions;
-
-export default auth.reducer;
+export const { setUser } = auth.actions
+export default auth.reducer
