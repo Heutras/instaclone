bed7440597d3c879864635740fe92d371f626b3d
Heutras <alperceltr@gmail.com>
2022-07-26 03:22:37 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux'
-import { useRoutes } from "react-router-dom";
+import {useRoutes} from "react-router-dom";
 import routes from "routes";
-import { Toaster } from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
+import {useSelector} from "react-redux";
 import Loader from "components/Loader";
+
 function App() {
-  const user = useSelector(state => state.auth.user)
-  const showRoutes = useRoutes(routes);
-  const [redirect, setRedirect] = useState(false);
 
-  useEffect(() => {
-    let timeout = setTimeout( ()=> {
-      setRedirect(true);
-    },2000)
-  
-    return () => {
-      clearTimeout(timeout);
-    }
-  }, [])
-  
-  if(!user && !redirect) {
-    return <Loader/>
-  }
+	const user = useSelector(state => state.auth.user)
+	const showRoutes = useRoutes(routes)
 
-  return(
-    <>
-      <Toaster position="top-right"/>
-      {showRoutes}
-    </>
-    )
-  
+	if (user === null) {
+		return <Loader />
+	}
+
+	return (
+		<>
+			<Toaster position="top-right" />
+			{showRoutes}
+		</>
+	)
 }
 
 export default App;
